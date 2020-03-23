fce174c9a5d6abb7cf3912481dea4b8e1bb9059a
Daniel Angulo <dangulo4@gmail.com>
2020-03-22 20:55:50 -0700

    connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES('Bacon Cheese Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES('Tripple Meat Double Bacon Burger');
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES('The Western Legend Burger', true);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES('The Memphis Ultimate Burger', true);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES('Octopus Blue Cheese Burger', true);
+
+INSERT INTO burgers
+    (burger_name)
+VALUES('The Hurt Burger');
