2fa89f7ce6d19a606d97e6f25776e2b194fc8c77
Daniel Angulo <dangulo4@gmail.com>
2020-03-23 12:07:58 -0700

    orm.js and burger_controllers.js

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,98 @@
-// Import connection.js
+// Import MySQL connection
+var connection = require('./connection');
 
-// Select All, insert One, update one
+// Helper function for SQL syntax
+function printQuestionMarks(num) {
+  var arr = [];
 
+  for (var i = 0; i < num; i++) {
+    arr.push('?');
+  }
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through keys and push key/value as string into array
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string has spaces, add quotes
+      if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + '=' + value);
+    }
+  }
+  // translate array of strings to single comma-seperated string
+  return arr.toString();
+}
+
+// Display all burgers
+var orm = {
+  selectAll: function(tableInput, cb) {
+    var queryString = 'SELECT * FROM ' + tableInput + ';';
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // Add one burger
+  insert: function(table, cols, vals, cb) {
+    var queryString = 'INSERT INTO ' + table;
+
+    queryString += ' (';
+    queryString += cols.toString();
+    queryString += ') ';
+    queryString += 'VALUES (';
+    queryString += printQuestionMarks(val.length);
+    queryString += ') ';
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // Updated burger status to devoured
+  update: function(table, objColVals, condition, cb) {
+    var queryString = 'UPDATE ' + table;
+
+    queryString += 'SET';
+    queryString += objToSql(objColVals);
+    queryString += 'WHERE';
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // Delete burger
+  delete: function(table, condition, cb) {
+    var queryString = 'DELETE FROM ' + table;
+    queryString += 'WHERE';
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
 // export module exports
+module.exports = orm;
--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,3 +1,68 @@
-// Import Express, burger.js
+// Import Dependencies
+var express = require('express');
+var router = express.Router();
 
-// Create the router and export router
+// Import the model
+var burger = require('../models/burger');
+
+// Create all routes with logic
+router.get('/', function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+// Add burger to db
+router.post('/api/burgers', function(req, res) {
+  burger.insert(
+    ['burger_name', 'devoured'],
+    [req.body.name, req.body.devoured],
+    function(result) {
+      // send back the ID
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+// Update existing burger
+router.put('/api/burgers/:id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows == 0) {
+        // if no rows changed, send 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
+// Delete burger from db
+router.delete('/api/burgers/:id', function(req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows changed, send 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export route for server.js
+module.exports = router;
